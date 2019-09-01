bbde458c17a342f7795ea84d0073acef70f3bba4
albseb511 <albertsebastian511@gmail.com>
2019-09-01 14:52:50 +0530

    bubble-color

--- algo/bubble/sort.js
+++ algo/bubble/sort.js
@@ -7,17 +7,20 @@ function sleep(ms) {
   }
 
 async function _bubble(){
+    var cont = document.getElementsByClassName('array-item')
     for(let i=0;i<arr.length;i++){
         for(let j=0;j<arr.length-1-i;j++){
             var swap= []
+            cont[j].style.background = 'green'
             if(arr[j]>arr[j+1])
             {
+                cont[j].style.background = 'yellow'
                 swap = __swap(arr[j],arr[j+1])
                 console.log(swap)
                 arr[j] = swap[0]
                 arr[j+1] = swap[1]
                 console.log(swap,arr,j,i)
-                await sleep(1000)
+                await sleep(1500)
                 _visualiseArray()
             }
             else null;
