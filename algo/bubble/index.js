6b4a362d735ae8ebd9e94999c2bcd7683faccbd5
albseb511 <albertsebastian511@gmail.com>
2019-09-01 14:46:16 +0530

    bubble await-async

--- algo/bubble/index.js
+++ algo/bubble/index.js
@@ -1,4 +1,4 @@
-var size = 50
+var size = 10
 var min = 0
 var max = 100
 var arr = new Array(size).fill(0)
@@ -6,3 +6,5 @@ var arr = new Array(size).fill(0)
 _tabBar()
 _arrayFill()
 _visualiseArray()
+
+_bubble()
--- algo/bubble/sort.js
+++ algo/bubble/sort.js
@@ -1,3 +1,28 @@
-function _bubble(arr){
-    
+function __swap(a,b){
+    return [b,a]
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function _bubble(){
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            var swap= []
+            if(arr[j]>arr[j+1])
+            {
+                swap = __swap(arr[j],arr[j+1])
+                console.log(swap)
+                arr[j] = swap[0]
+                arr[j+1] = swap[1]
+                console.log(swap,arr,j,i)
+                await sleep(1000)
+                _visualiseArray()
+            }
+            else null;
+            
+        }
+    }
+}
+
